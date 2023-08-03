9f6de08226ea58786b6b35daf001c2a8488bbabd
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-08-03 21:16:00 +0530

    a

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ app.post('/generate', async (req, res) => {
   }
 });
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send("home route ")
 });
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
